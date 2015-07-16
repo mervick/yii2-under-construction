6fcfc64abc0cc0f329693ce057d2873125a29f6c
Andrey Izman <izman.dev@gmail.com>
2015-07-17 00:40:32 +0300

    Add assets

---
+++ assets/underconstruction.js
@@ -0,0 +1,11 @@
+(function($) {
+    $(document).ready(function () {
+        // Init CanvasBG and pass target starting location
+        CanvasBG.init({
+            Loc: {
+                x: window.innerWidth / 2.1,
+                y: window.innerHeight / 2.2
+            },
+        });
+    });
+}) (jQuery);
