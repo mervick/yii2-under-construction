3b567adfa40f73761eeb44fa4916842aa25a2dab
Andrey Izman <izman.dev@gmail.com>
2015-07-18 23:06:03 +0300

    Add l18n text

--- views/default/index.php
+++ views/default/index.php
@@ -1,12 +1,17 @@
 <?php
-$this->title = 'Site is Under Construction!'
+
+use Yii;
+use yii\helpers\Html;
+$this->title = Html::encode(Yii::t('underconstruction', 'Site Under Construction!'));
 ?>
 <div class="content">
     <div class="logo"></div>
     <div class="container">
-        <h1>Site Under Construction!</h1>
-        <p>Our site is under construction and will be ready for some time.</p>
-        <p>Our contacts:</p>
-        <p>E-mail: <a href="mailto:<?= Yii::$app->params['adminEmail'] ?>"><?= Yii::$app->params['adminEmail'] ?></a></p>
+        <h1><?= Yii::t('underconstruction', 'Site Under Construction!') ?></h1>
+        <p><?= Yii::t('underconstruction', 'Our site is under construction and will be ready for some time.') ?></p>
+        <p><?= Yii::t('underconstruction', 'Our contacts:') ?></p>
+        <p><?= Yii::t('underconstruction', 'E-mail:') ?>
+            <a href="mailto:<?= Yii::$app->params['adminEmail'] ?>"><?= Yii::$app->params['adminEmail'] ?></a>
+        </p>
     </div>
 </div>
