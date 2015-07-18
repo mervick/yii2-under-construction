e271c740d5989bc9eeea2419fd2f120d2d32e447
Andrey Izman <izman.dev@gmail.com>
2015-07-18 23:21:41 +0300

    Add ukrainian translation

---
+++ messages/uk_UA/underconstruction.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Site Under Construction!' => 'Сайт знаходиться в розробці!',
+    'Our site is under construction and will be ready for some time.' =>
+        'Наш сайт знаходиться в стадії розробки і буде доступний через деякий час, спробуйте зайти пізніше.',
+    'Our contacts:' => 'Наші контакти:',
+    'E-mail:' => 'Е-майл:',
+];
