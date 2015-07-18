4842153e7591f48092f8145486bcbc9a0bbdbd26
Andrey Izman <izman.dev@gmail.com>
2015-07-18 23:33:40 +0300

    Add english translation

---
+++ messages/en-US/underconstruction.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Site Under Construction!' => 'Site Under Construction!',
+    'Our site is under construction and will be ready for some time.' => 'Our site is under construction and will be ready for some time.',
+    'Our contacts:' => 'Our contacts:',
+    'E-mail:' => 'E-mail:',
+];
