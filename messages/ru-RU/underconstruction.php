f6c15a5ea65f186ec72ba2b8253113295f939529
Andrey Izman <izman.dev@gmail.com>
2015-07-18 23:17:25 +0300

    Create russian translation

---
+++ messages/ru-RU/underconstruction.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Site Under Construction!' => 'Сайт находится в разработке!',
+    'Our site is under construction and will be ready for some time.' => 'Наш сайт находится в стадии разработки и будет доступен через некоторое время, попробуйте зайти позже.',
+    'Our contacts:' => 'Наши контакты:',
+    'E-mail:' => 'E-mail:',
+];
