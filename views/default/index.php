059103d09f196a8340ddc37c2ab294109c4482f4
Andrey Izman <izman.dev@gmail.com>
2015-07-18 23:15:15 +0300

    Update index.php

--- views/default/index.php
+++ views/default/index.php
@@ -2,15 +2,15 @@
 
 use Yii;
 use yii\helpers\Html;
-$this->title = Html::encode(Yii::t('underconstruction', 'Site Under Construction!'));
+$this->title = Html::encode(Yii::t('modules/underconstruction', 'Site Under Construction!'));
 ?>
 <div class="content">
     <div class="logo"></div>
     <div class="container">
-        <h1><?= Yii::t('underconstruction', 'Site Under Construction!') ?></h1>
-        <p><?= Yii::t('underconstruction', 'Our site is under construction and will be ready for some time.') ?></p>
-        <p><?= Yii::t('underconstruction', 'Our contacts:') ?></p>
-        <p><?= Yii::t('underconstruction', 'E-mail:') ?>
+        <h1><?= Yii::t('modules/underconstruction', 'Site Under Construction!') ?></h1>
+        <p><?= Yii::t('modules/underconstruction', 'Our site is under construction and will be ready for some time.') ?></p>
+        <p><?= Yii::t('modules/underconstruction', 'Our contacts:') ?></p>
+        <p><?= Yii::t('modules/underconstruction', 'E-mail:') ?>
             <a href="mailto:<?= Yii::$app->params['adminEmail'] ?>"><?= Yii::$app->params['adminEmail'] ?></a>
         </p>
     </div>
